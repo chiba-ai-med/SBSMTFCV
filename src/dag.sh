3976cbd4f8f2ddb1d43ae9e87dfc4e4dfee5734c
Koki Tsuyuzaki <k.t.the-answer@hotmail.co.jp>
2024-06-11 12:10:13 +0900

    fix: DAG

--- src/dag.sh
+++ src/dag.sh
@@ -1,8 +1,4 @@
 # DAG graph
 mkdir -p plot
 snakemake --rulegraph \
---config input=data/testdata.tsv outdir=output \
-rank_min=2 rank_max=10 \
-lambda_min=-10 lambda_max=10 trials=10 \
-n_iter_max=100 x_new_list="" bin_h=TRUE bin_w_new=TRUE \
-beta=2 ratio=5 | dot -Tpng > plot/dag.png
+--config input=data/testdata.tsv outdir=output | dot -Tpng > plot/dag.png
