a0d600e307b8bd052b00a75a6108baa62e3b726b
Koki Tsuyuzaki <k.t.the-answer@hotmail.co.jp>
2024-07-04 11:21:44 +0900

    fix: {-1,1}=>{0,1}

--- src/bindata_for_landscaper.R
+++ src/bindata_for_landscaper.R
@@ -5,8 +5,8 @@ outfile <- args[2]
 # Loading
 U <- as.matrix(read.table(infile, header=FALSE))
 
-# {0,1} => {-1,1}
-U[which(U < 0.5)] <- -1
+# Binarization
+U[which(U < 0.5)] <- 0
 U[which(U >= 0.5)] <- 1
 
 # Save
